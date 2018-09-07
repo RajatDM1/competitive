0437edc0712a82e8e4353c5d7f1b9c7fcbaf5cbd
RajatDM1 <chilled.bira91@yandex.com>
2018-09-07 22:21:30 +0530

    solutions

---
+++ solutions/codeforces/520A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 100001
+#define lli long long int
+#define VI <vector <int> >
+#define pb push_back 
+int main(){
+	int n;
+	cin>>n;
+	cin.ignore();
+	string s;
+	getline(cin,s);
+	transform(s.begin(),s.end(),s.begin(),::tolower);
+	cout<<s<<endl;
+	// for(int i =0 ;i<n;i++)
+	// {
+	// 	occ[s[i]-'0']++;
+	// }	
+	// // while(i<27)
+	// // {
+	// // 	if()
+	// // }
+	return 0;
+}
