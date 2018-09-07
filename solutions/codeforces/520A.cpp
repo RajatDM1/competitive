043da054f2a27e92a758e6750730f6bc2b68aff7
RajatDM1 <chilled.bira91@yandex.com>
2018-09-07 23:08:07 +0530

    solutions

--- solutions/codeforces/520A.cpp
+++ solutions/codeforces/520A.cpp
@@ -5,20 +5,29 @@ using namespace std;
 #define VI <vector <int> >
 #define pb push_back 
 int main(){
-	int n;
+	int occ[26]={0};
+	int n,flag=0;
 	cin>>n;
 	cin.ignore();
 	string s;
 	getline(cin,s);
+	// cin>>s;
 	transform(s.begin(),s.end(),s.begin(),::tolower);
-	cout<<s<<endl;
-	// for(int i =0 ;i<n;i++)
-	// {
-	// 	occ[s[i]-'0']++;
-	// }	
-	// // while(i<27)
-	// // {
-	// // 	if()
-	// // }
+	// cout<<s<<endl;
+	for(int i =0 ;i<n;i++)
+	{
+		occ[s[i]-'a']++;
+	}	
+	int i=0;
+	while(i<26)
+	{
+		// cout<<s[i]<<",";
+		if(occ[i++]==0)
+			{
+				// cout<<s[i]<<endl;
+				flag = 1;
+			}
+	}
+	flag?cout<<"NO":cout<<"YES";
 	return 0;
 }
