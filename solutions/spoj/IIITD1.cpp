20a0e95075588caa9047c1eb660a040eaa8617dc
RajatDM1 <chilled.bira91@yandex.com>
2018-10-09 22:30:03 +0530

    solutions

---
+++ solutions/spoj/IIITD1.cpp
@@ -0,0 +1,78 @@
+		//not my tamplate
+#include <vector> 
+#include <queue>
+#include <map> 
+#include <set>
+#include <utility> //Pair
+#include <algorithm>
+#include <sstream> // istringstream>> ostring stream<<
+#include <iostream> 
+#include <iomanip> 
+//setbase - cout << setbase (16); cout << 100 << endl; Prints 64
+//setfill -   cout << setfill ('x') << setw (5); cout << 77 << endl; prints xxx77
+//setprecision - cout << setprecision (4) << f << endl; Prints x.xxxx
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <cassert>
+#include <fstream>
+#include <limits>
+using namespace std;
+ 
+typedef long long ll;
+typedef vector <int> vi;
+typedef pair< int ,int > pii;
+typedef istringstream iss;
+typedef ostringstream oss;
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define sz size()
+#define ln length()
+#define rep(i,n) for(int i=0;i<n;i++)
+#define fu(i,a,n) for(int i=a;i<=n;i++)
+#define fd(i,n,a) for(int i=n;i>=a;i--)
+#define all(a)  a.begin(),a.end() 
+ 
+#define gi(n) scanf("%d",&n)
+#define gll(n) scanf("%lld",&n);
+#define gl(n) cin >> n
+#define pi(n) printf("%d",n)
+#define pll(n) printf("%lld",n)
+#define pl(n) cout << n
+#define ps printf(" ")
+#define pn printf("\n")
+#define dg(n,s); printf("%s %d",s,n)
+#define imax numeric_limits<int>::max()
+#define imin numeric_limits<int>::min()
+#define lmax numeric_limits<ll>::max()
+#define lmin numeric_limits<ll>::min()
+int main() {
+	int t,n,rem;
+	scanf("%d",&t);
+	while(t--)
+	{
+		int j=0;
+		// vector<int> num;
+		scanf("%d",&n);
+		if(n<0)
+		{
+			j = abs(n)-n;
+		}
+		int ans = 1;
+		while(n/10)
+		{
+			n = n/10;
+			ans = ans*10;
+		}
+		// num.pb(1);
+		// reverse(num.begin(),num.end());
+		// int ans = 0;
+		// for(int i=0;i<num.size();i++){ans = ans*10+num[i];}
+		cout<<ans+j<<endl;
+	}
+	return 0;
+}
